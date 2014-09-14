95e58388dceb7145f0e167898d012cbb79bf5838
Teo Klestrup Röijezon <teo@nullable.se>
2014-09-14 16:48:07 +0200

    Start writing plugin

---
+++ src/main/groovy/se/nullable/squeeze/SqueezePlugin.groovy
@@ -0,0 +1,10 @@
+package se.nullable.squeeze
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SqueezePlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+    }
+}
